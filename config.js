7bcc9c4b3a60eb65ea3f9c9a410d81ed46c911e1
SKYLLERBOTZ <bibilmakna1577@gmail.com>
2023-06-15 11:13:18 +0700

    y

---
+++ config.js
@@ -0,0 +1,67 @@
+/*
+    Created & Base By ArxzyDev
+    Jual Sc? Neraka Paling Bawah
+    My Contact https://wa.me/6289513081052
+    
+   Notes:
+   Jika Ingin Recode Silakan Tapi Ingat Creditnya
+   Sc Adrian-MD Akan Terus Di Update.
+   Jika Ingin Beli Apikey Chat Owner Lolhuman
+   
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '' // AMBIL DI LOLHUMAN
+global.rosekey = 'cba86fac49e37d63f9bd4561' // ROSE
+
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'hikachu'
+global.namaowner = 'Hika'
+global.wagc = "https://chat.whatsapp.com/KpzmWpuhydXDZCZlm6XLPD"
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6285882135288']
+global.nomerowner = '6285882135288'
+global.premium = ['6285882135288']
+
+//—————「 Set Wm 」—————//
+global.packname = 'hikachu'
+global.author = 'Bot: +62 858 8213 5288\nIG: sky4you\nInstagram: sky4you'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    success: '🤗Done, Oke Desu~',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    botAdmin: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: '⏳ Sedang Di Proses !',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !\n\n🎯 *Premium Cuma 3k Permanen* 😋\n\n🍂 *Subscribe Channel Owner Pakai 10 Akun Gratis Premium Bot (Sertakan Bukti)* ❗',
+    error: '🚫 Fitur Sedang Error !',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/bcb6c6bfa6dcb7c87bd78.jpg'
+global.link = 'https://instagram.com/sky4you'
+global.thumb = fs.readFileSync('./media/koneko.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
